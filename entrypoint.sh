d3f6c34faa86e2258b507163caa28c6a1928fece
Corey Larson <corey@earthly.dev>
2021-02-03 03:57:54 -0700

    All absolute

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,16 @@
 
 case "$1" in
     "--key")
-    cat ./key.pem
+    cat /aws/key.pem
     ;;
     "--destroy")
-    exec ./delete.sh
+    exec /aws/delete.sh
     ;;
     "--resources")
-    jq ./resources.json
+    jq . /aws/resources.json
     ;;
     "--proxy-cmd")
-    ssh -i key.pem -R 8080:localhost:3000 ec2-user@$(jq .EC2_PUBLIC ./resources.json)
+    ssh -i key.pem -R 8080:localhost:3000 ec2-user@$(jq .EC2_PUBLIC /aws/resources.json)
     ;;
     *)
     echo "Valid options: --key / --destroy / --resources / --proxy-cmd"
